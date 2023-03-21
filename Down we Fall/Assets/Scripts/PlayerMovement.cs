358ec27d40f5d34695242ed1c7482abadd9fdab9
Máximo Fain <maximofain0@gmail.com>
2023-03-21 00:12:13 -0300

    Player and Jump progress
    Added Player and Idle animation. Started to develop jump mechanic

--- Down we Fall/Assets/Scripts/PlayerMovement.cs
+++ Down we Fall/Assets/Scripts/PlayerMovement.cs
@@ -6,15 +6,30 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private float xDir;
+    private float yDir;
     private Rigidbody2D _rb;
     [SerializeField] private float _speed = 10;
+    [SerializeField] private float jumpPower = 5;
     
     void Start()
     {
         _rb = gameObject.GetComponent<Rigidbody2D>();
     }
-    
-    void Update()
+
+    void Jump()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            yDir = 1;
+            print("Space is getting down");
+        }
+        else
+        {
+            yDir = 0;
+        }
+    }
+
+    void XMovement()
     {
         if (Input.GetKey(KeyCode.A))
         {
@@ -29,9 +44,14 @@ public class PlayerMovement : MonoBehaviour
             xDir = 0;
         }
     }
+    void Update()
+    {
+        XMovement();
+        Jump();
+    }
 
     private void FixedUpdate()
     {
-        _rb.AddForce(new Vector2(xDir * _speed ,0));
+        _rb.AddForce(new Vector2(xDir * _speed ,yDir * jumpPower));
     }
 }
